8c21a9b3e75c5c60ec558d26797288aee1999984
Naman Jain <90482311+Shineuptillast@users.noreply.github.com>
2023-02-08 22:06:54 +0530

    Add files via upload

---
+++ mod2.py
@@ -0,0 +1,9 @@
+def f21(a):
+    """This function gives n raise to the power of 2"""
+    return 2**a
+def f22(a):
+    """This function gives the number multiplited by 10"""
+    return a*10
+def f23(a,b):
+    """This function gives the larger of two number"""
+    return a if a>b else b
